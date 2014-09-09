323d5fc79be9669858eddd8ceabe224a2342dc89
flpf <fpfeifle@gmail.com>
2014-09-09 19:17:20 +0200

    Adding Hilbert

---
+++ src/hilbert.jl
@@ -0,0 +1,9 @@
+#Hilbert-transform. Kammeyer, K.D.: Digitale Signalverarbeitung. Teubner. 2006. pp.28 ff.
+function hilbert{T<:Number}(σ::AbstractVector{T})
+    σₙ=length(σ);
+    σₕ=div(σₙ,2);
+		σ=complex(2*σ)
+    FFTW.fft!(σ)
+    σ[1]/=0
+    FFTW.ifft!([σ[1:σₕ];zeros(σₕ)])
+end
