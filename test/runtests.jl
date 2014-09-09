e82277ff0781b51ebeb9bfb028e7e139f85465d7
flpf <fpfeifle@gmail.com>
2014-09-09 12:09:00 +0200

    Testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using WignerVille
 using Base.Test
 
-chirp=readdlm("../files/chirp.txt") # 8000 sample long chirp produced with MATLAB
-results=readdlm("../files/results_wvt.txt")
+chirp=readdlm("../files/chirp.txt") # 8000 sample long chirp produced in MATLAB with the chirp()
+
+results=readdlm("../files/results_wvt.txt") # The results of the wigner ville transform from MATLAB
+
 
 @test wvt(chirp) == results
