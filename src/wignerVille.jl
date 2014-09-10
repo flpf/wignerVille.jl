53abf0fad8b34283b74894327b075463cdd41466
flpf <fpfeifle@gmail.com>
2014-09-10 09:00:15 +0200

    Starting the wvt

--- src/wignerVille.jl
+++ src/wignerVille.jl
@@ -1,2 +1,19 @@
-function wignerVille()
+include("hilbert.jl")
+
+function wignerVille{T<:Number}(σ::AbstractVector{T},samplingFrequency::Int,tsWinL::Int,fsWinL::Int)
+Ψ = hilbert(σ);	
+σn=lenght(σ);
+σp=div(σn,tsWinL);
+
+end
+
+function pseudoWignerVille{T<:Number}(s::AbstractVector{T},samplingFrequency::Int,fsWinL::Int)
+
+	
+
+end
+
+function smoothedPseudoWignerVille{T<:Number}(s::AbstractVector{T},samplingFrequency::Int,tsWinL::Int,fsWinL::Int)
+	
+
 end
