fe69c2cd3975c5330f2c68e605ad8e88e2ab4486
flpf <fpfeifle@gmail.com>
2014-09-10 23:58:51 +0200

    first spwv-does not work correct

--- src/wignerVille.jl
+++ src/wignerVille.jl
@@ -3,39 +3,54 @@ include("hilbert.jl")
 function wignerVille{T<:Number}(σ::AbstractVector{T},ω::Int,τ::Int)
 #The Wigner-Ville transform without windowing.
 #
-Φ = hilbert(σ)	
-σn=length(σ)
-σp=div(σn,τ)
-τₕ=div(τ,2)
-Ψ=zeros(Complex,ω,σn-τₕ)
-count=1;
-for ι=1+τₕ:σn-τₕ-1
-	Ψ[:,count]=[Φ[ι-τₕ:ι+τₕ].*conj(Φ[ι+τₕ:-1:ι-τₕ]); zeros(Complex,ω-τ-1)];
-count+=1;	
-end
-FFTW.fft(Ψ,1)
+  Φ = hilbert(σ)	
+  σn=length(σ)
+  σp=div(σn,τ)
+  τₕ=div(τ,2)
+  Ψ=zeros(Complex,ω,σn-τₕ)
+  count=1;
+  for ι=1+τₕ:σn-τₕ-1
+    Ψ[:,count]=[Φ[ι-τₕ:ι+τₕ].*conj(Φ[ι+τₕ:-1:ι-τₕ]); zeros(Complex,ω-τ-1)];
+    count+=1;	
+  end
+  FFTW.fft(Ψ,1)
 end
 
 function pseudoWignerVille{T<:Number}(σ::AbstractVector{T},ω::Int,τ::Int,ξ::Int)
 #Pseudo-Wigner-Ville transform with Gauss windowing.
-Φ=hilbert(σ)	
-σn=length(σ)
-σp=div(σn,τ)
-Ψ=zeros(Complex,ω,σn)
-τₕ=div(τ,2)
-count=1;
-Ω=gaussWindow(τ,τₕ,ξ);
-Ωₕ=hilbert(Ω)
-for ι=1+τₕ:σn-τₕ-1
-	Ψ[:,count]=[Ωₕ[:].*conj(Ωₕ[:]).*Φ[ι-τₕ:ι+τₕ-1].*conj(Φ[ι+τₕ-1:-1:ι-τₕ]); zeros(Complex,ω-τ)];
-count+=1;
-end
-FFTW.fft(Ψ,1)
+  Φ=hilbert(σ)	
+  σn=length(σ)
+  σp=div(σn,τ)
+  Ψ=zeros(Complex,ω,σn)
+  τₕ=div(τ,2)
+  count=1;
+  Θ=gaussWindow(τ,τₕ,ξ);
+  Θₕ=hilbert(Ω)
+  for ι=1+τₕ:σn-τₕ-1
+    Ψ[:,count]=[Θₕ[:].*conj(Θₕ[:]).*Φ[ι-τₕ:ι+τₕ-1].*conj(Φ[ι+τₕ-1:-1:ι-τₕ]); zeros(Complex,ω-τ)];
+    count+=1;
+  end
+  FFTW.fft(Ψ,1)
 end
 
-function smoothedPseudoWignerVille{T<:Number}(s::AbstractVector{T},samplingFrequency::Int,tsWinL::Int,fsWinL::Int)
-	
-
+function smoothedPseudoWignerVille{T<:Number}(σ::AbstractVector{T},ω::Int,τ::Int,ξ::Int,β::Int,Ξ::Int)
+  Φ=hilbert(σ)	
+  σn=length(σ)
+  σp=div(σn,τ)
+  Ψ=zeros(Complex,ω,σn)
+  τₕ=div(τ,2)
+  βₕ=div(β,2)
+  count=1;
+  Θ=gaussWindow(τ,τₕ,ξ);
+  Θₕ=hilbert(Θ)
+  Ω=gaussWindow(β,βₕ,Ξ);
+  Ωₕ=hilbert(Ω)
+  for ι=1+τₕ:σn-τₕ-1
+	Ψₜ=sum(Θₕ[:].*conj(Θₕ[:]).*Φ[ι-τₕ:ι+τₕ-1].*conj(Φ[ι+τₕ-1:-1:ι-τₕ]))
+    Ψ[:,count]=[Ω[:].*Ψₜ; zeros(Complex,ω-β)];
+    count+=1;
+  end
+  FFTW.fft(Ψ,1) 	
 end
 
 function gaussWindow{T<:Int}(τ::T,ξ::T,Δ::T,γ=1::T)
