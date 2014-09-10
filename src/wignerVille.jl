c13c949c3a538ff79c67407fb4a0926387fb8ca9
flpf <fpfeifle@gmail.com>
2014-09-10 23:27:16 +0200

    wvt and pwvt seem to be working. Scaling is still wrong

--- src/wignerVille.jl
+++ src/wignerVille.jl
@@ -1,23 +1,36 @@
 include("hilbert.jl")
 
 function wignerVille{T<:Number}(σ::AbstractVector{T},ω::Int,τ::Int)
+#The Wigner-Ville transform without windowing.
+#
 Φ = hilbert(σ)	
+σn=length(σ)
+σp=div(σn,τ)
+τₕ=div(τ,2)
+Ψ=zeros(Complex,ω,σn-τₕ)
+count=1;
+for ι=1+τₕ:σn-τₕ-1
+	Ψ[:,count]=[Φ[ι-τₕ:ι+τₕ].*conj(Φ[ι+τₕ:-1:ι-τₕ]); zeros(Complex,ω-τ-1)];
+count+=1;	
+end
+FFTW.fft(Ψ,1)
+end
+
+function pseudoWignerVille{T<:Number}(σ::AbstractVector{T},ω::Int,τ::Int,ξ::Int)
+#Pseudo-Wigner-Ville transform with Gauss windowing.
+Φ=hilbert(σ)	
 σn=length(σ)
 σp=div(σn,τ)
 Ψ=zeros(Complex,ω,σn)
 τₕ=div(τ,2)
 count=1;
-		for ι=1+τₕ:σn-τₕ-1
-			Ψ[:,count]=[Φ[ι-τₕ:ι+τₕ].*conj(fliplr(Φ[ι-τₕ:ι+τₕ])); zeros(Complex,ω-τ-1)];
-		count+=1;	
-    end
- FFTW.fft(Ψ,1)
+Ω=gaussWindow(τ,τₕ,ξ);
+Ωₕ=hilbert(Ω)
+for ι=1+τₕ:σn-τₕ-1
+	Ψ[:,count]=[Ωₕ[:].*conj(Ωₕ[:]).*Φ[ι-τₕ:ι+τₕ-1].*conj(Φ[ι+τₕ-1:-1:ι-τₕ]); zeros(Complex,ω-τ)];
+count+=1;
 end
-
-function pseudoWignerVille{T<:Number}(s::AbstractVector{T},samplingFrequency::Int,fsWinL::Int)
-
-	
-
+FFTW.fft(Ψ,1)
 end
 
 function smoothedPseudoWignerVille{T<:Number}(s::AbstractVector{T},samplingFrequency::Int,tsWinL::Int,fsWinL::Int)
