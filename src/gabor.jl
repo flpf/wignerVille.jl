e04fabbcb15e6e2d0925c7274c636b9173931cb9
flpf <fpfeifle@gmail.com>
2015-02-17 14:32:20 +0100

    Update gabor.jl

--- src/gabor.jl
+++ src/gabor.jl
@@ -3,12 +3,12 @@ function dgt{T<:Number}(σ::AbstractVector{T},ω::Int,τ::Int)
 	σₙ=length(σ);
 	σ=conv(σ,fliplr(σ));
 	Δ=div(σₙ,τ);
-  τₕ=div(τ,2);
+	τₕ=div(τ,2);
 	Γ=zeros(ω,σₙ);
 	σ=[zeros(τₕ); σ; zeros(τₕ)];
-  Θ=gaussWindow(τ,τₕ,τₕ);
+	Θ=gaussWindow(τ,τₕ,τₕ);
 	for r=1:σₙ, p=0:τ-1
-		 Γ[p+1,r]=Θ[p+1].*σ[r+p];
+		Γ[p+1,r]=Θ[p+1].*σ[r+p];
 	end
 	abs(FFTW.fft(Γ,1))
 end
