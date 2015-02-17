5f88fcd9e897e08a40814daedced94f8060ffd07
flpf <fpfeifle@gmail.com>
2015-02-17 14:31:16 +0100

    Update gabor.jl
    Cleaning up.

--- src/gabor.jl
+++ src/gabor.jl
@@ -1,13 +1,12 @@
 include("Windows.jl")
-
 function dgt{T<:Number}(σ::AbstractVector{T},ω::Int,τ::Int)
 	σₙ=length(σ);
 	σ=conv(σ,fliplr(σ));
 	Δ=div(σₙ,τ);
-    τₕ=div(τ,2);
+  τₕ=div(τ,2);
 	Γ=zeros(ω,σₙ);
-    σ=[zeros(τₕ); σ; zeros(τₕ)];
-    Θ=gaussWindow(τ,τₕ,τₕ);
+	σ=[zeros(τₕ); σ; zeros(τₕ)];
+  Θ=gaussWindow(τ,τₕ,τₕ);
 	for r=1:σₙ, p=0:τ-1
 		 Γ[p+1,r]=Θ[p+1].*σ[r+p];
 	end
